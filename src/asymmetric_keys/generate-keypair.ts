9af4834c9174a52e379a9cb48ba97c8a4e5de220
mashk4 <99094113+mashk4@users.noreply.github.com>
2024-08-08 21:08:39 +0300

    Add check_balance.ts

--- src/asymmetric_keys/generate-keypair.ts
+++ src/asymmetric_keys/generate-keypair.ts
@@ -1,9 +1,9 @@
-import "dotenv/config";
-import { Keypair } from "@solana/web3.js";
+import 'dotenv/config';
+import { Keypair } from '@solana/web3.js';
 
-let privateKey = process.env["SECRET_KEY"];
+let privateKey = process.env['SECRET_KEY'];
 if (privateKey === undefined) {
-  console.log("Add SECRET_KEY to .env!");
+  console.log('Add SECRET_KEY to .env!');
   process.exit(1);
 }
 const asArray = Uint8Array.from(JSON.parse(privateKey));
---
+++ src/reading_from_blockchain/check_balance.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl
+} from '@solana/web3.js';
+
+const connection = new Connection(clusterApiUrl('devnet'));
+console.log('Connection established');
+
+let publicKeyString = process.env['PUBLIC_KEY'];
+if (publicKeyString === undefined) {
+  console.log('Add PUBLIC_STRING to .env!');
+  process.exit(1);
+}
+
+const publicKey = new PublicKey(publicKeyString);
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(`💰 The balance for the wallet at address ${publicKey} is: ${balanceInSOL}`)
